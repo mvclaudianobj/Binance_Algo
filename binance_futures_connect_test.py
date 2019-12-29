2c042e82b80350743b91c3827200118227b281ec
drdong <szdongofficial@gmail.com>
2019-12-30 06:31:22 +0900

    サンプルを一つ追加した

--- binance_futures_connect_test.py
+++ binance_futures_connect_test.py
@@ -15,8 +15,19 @@ def main():
                 'private': 'https://fapi.binance.com/fapi/v1',
             }, }
     })
+
+    # 取引ペア詳細の一覧
     print(json.dumps(binance_futures.load_markets(), indent=4))
 
+    # 新規注文
+    print(binance_futures.create_order("BTC/USDT", "LIMIT", "BUY", 0.01, 7100))
+    # {'info': {'orderId': 467051443, 'symbol': 'BTCUSDT', 'status': 'NEW', 'clientOrderId': 'oYw35DTndBBxPEBqxTVUcm',
+    #           'price': '7100', 'avgPrice': '0.00000', 'origQty': '0.010', 'executedQty': '0', 'cumQty': '0',
+    #           'cumQuote': '0', 'timeInForce': 'GTC', 'type': 'LIMIT', 'reduceOnly': False, 'side': 'BUY',
+    #           'stopPrice': '0', 'workingType': 'CONTRACT_PRICE', 'origType': 'LIMIT', 'updateTime': 1577654973723},
+    #  'id': '467051443', 'timestamp': None, 'datetime': None, 'lastTradeTimestamp': None, 'symbol': 'BTC/USDT',
+    #  'type': 'limit', 'side': 'buy', 'price': 7100.0, 'amount': 0.01, 'cost': 0.0, 'average': None, 'filled': 0.0,
+    #  'remaining': 0.01, 'status': 'open', 'fee': None, 'trades': None}
 
 
 if __name__ == '__main__':
