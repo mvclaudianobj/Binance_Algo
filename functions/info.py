445e6d72229407affbde3defc8b48547018f7bcb
drdong <szdongofficial@gmail.com>
2019-12-31 00:51:20 +0900

    いくつか新しい関数を追加した

--- functions/info.py
+++ functions/info.py
@@ -22,6 +22,7 @@ class Info:
         })
 
     def line_notify(self, message, pic=False, path=None):
+        print(message)
         if self.line_notify_token != None:
             line_notify_api = 'https://notify-api.line.me/api/notify'
             message = "\n{}\n".format(message)
@@ -45,6 +46,30 @@ class Info:
     def get_Margin_Balance(self):
         return float(self.binance_ccxt.fetch_balance()["info"]["assets"][1]["marginBalance"])
 
+    def fetch_orders(self, orderID, symbol):
+        return self.binance_ccxt.fetch_order(id=orderID, symbol=symbol)
+
+    def judge_ma_slope(self, side, symbol, period, ma_period, method="C"):
+        method_lst = ["O", "H", "L", "C"]
+        method_dict = {"O": 1, "H": 2, "L": 3, "C": 4}
+        if method not in method_lst:
+            print("INCORRECT METHOD!!!")
+            exit()
+
+        ohlcv_lst = self.binance_ccxt.fetch_ohlcv(symbol, timeframe=period, limit=ma_period + 2)
+        if side == "sell":
+            if ohlcv_lst[0] > ohlcv_lst[len(ohlcv_lst) - 2]:
+                return {"slope": True,
+                        "var_slope": (ohlcv_lst[0] - ohlcv_lst[len(ohlcv_lst) - 2][method_dict[method]]) / ma_period}
+            else:
+                return {"slope": False}
+        else:
+            if ohlcv_lst[0] < ohlcv_lst[len(ohlcv_lst) - 2]:
+                return {"slope": True,
+                        "var_slope": (ohlcv_lst[len(ohlcv_lst) - 2][method_dict[method]] - ohlcv_lst[0]) / ma_period}
+            else:
+                return {"slope": False}
+
     def moving_average(self, symbol, period, ma_period, method="C"):
         method_lst = ["O", "H", "L", "C"]
         method_dict = {"O": 1, "H": 2, "L": 3, "C": 4}
@@ -59,3 +84,7 @@ class Info:
             sum_price += ohlcv_lst[i][method_dict[method]]
 
         return round(sum_price / ma_period, 3)
+
+    # def last_updated_time(self, symbol, period, ma_period):
+    #     ohlcv_lst = self.binance_ccxt.fetch_ohlcv(symbol, timeframe=period, limit=ma_period + 1)
+    #     return ohlcv_lst[len(ohlcv_lst) - 1][0]
