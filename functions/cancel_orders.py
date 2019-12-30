e76bd2afa2b2e6c84b0cb680337b8dc952587e51
drdong <szdongofficial@gmail.com>
2019-12-30 14:28:28 +0900

    パラメータの説明を追加し、完成してない部分をコメントアウトした

--- functions/cancel_orders.py
+++ functions/cancel_orders.py
@@ -3,10 +3,16 @@ class cancelOrders:
         self.binance_futures = binance_futures
 
     def cancel_one_order(self, orderId, symbol):
+        """
+
+        :param orderId: order ID, str
+        :param symbol: symbol, str
+        :return:
+        """
         return self.binance_futures.cancel_order(id=orderId, symbol=symbol)
 
-    def cancel_all_orders(self, symbol):
-        return self.binance_futures.cancel_order(symbol=symbol)
-
-    def cancel_some_orders(self, orderIds, symbol):
-        return self.binance_futures.cancel_order(id=orderIds, symbol=symbol)
+    # def cancel_all_orders(self, symbol):
+    #     return self.binance_futures.cancel_order(symbol=symbol)
+    #
+    # def cancel_some_orders(self, orderIds, symbol):
+    #     return self.binance_futures.cancel_order(id=orderIds, symbol=symbol)
