5b5fc7e2dd38c7ce868ac51dd353db9e50958d62
drdong <szdongofficial@gmail.com>
2019-12-30 14:24:15 +0900

    パラメータの説明を追加した

--- functions/create_orders.py
+++ functions/create_orders.py
@@ -4,14 +4,37 @@ class creatOrders:
         self.binance_futures = binance_futures
 
     def limit_order(self, side, amount, price, params={}):
+        """
+
+        :param side: "buy" or "sell", str
+        :param amount: quantity, float
+        :param price: limit price, float
+        :param params: {}, json
+        :return:
+        """
         return self.binance_futures.create_order(symbol=self.symbol, type="LIMIT", side=side, amount=amount,
                                                  price=price, params=params)
 
     def market_order(self, side, amount, params={}):
+        """
+
+        :param side: "buy" or "sell", str
+        :param amount: quantity, float
+        :param params: {}, json
+        :return:
+        """
         return self.binance_futures.create_order(symbol=self.symbol, type="MARKET", side=side, amount=amount,
                                                  params=params)
 
     def stop_limit_order(self, side, amount, price, triggerPrice):
+        """
+
+        :param side: "buy" or "sell", str
+        :param amount: quantity, float
+        :param price: stop limit price, float
+        :param triggerPrice: trigger price, float
+        :return:
+        """
         return self.binance_futures.create_order(symbol=self.symbol, type="STOP", side=side, amount=amount,
                                                  price=price, params={"stopPrice": triggerPrice})
 
@@ -20,9 +43,23 @@ class creatOrders:
     #                                              price=price, params={"stopPrice": triggerPrice})
 
     def stop_market_order(self, side, amount, triggerPrice):
+        """
+
+        :param side: "buy" or "sell", str
+        :param amount: quantity, float
+        :param triggerPrice: trigger price, float
+        :return:
+        """
         return self.binance_futures.create_order(symbol=self.symbol, type="STOP_MARKET", side=side, amount=amount,
                                                  params={"stopPrice": triggerPrice})
 
     def take_profit_market_order(self, side, amount, triggerPrice):
+        """
+
+        :param side: "buy" or "sell", str
+        :param amount: quantity, float
+        :param triggerPrice: trigger price, float
+        :return:
+        """
         return self.binance_futures.create_order(symbol=self.symbol, type="TAKE_PROFIT_MARKET", side=side,
                                                  amount=amount, params={"stopPrice": triggerPrice})
